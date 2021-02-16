69fbf98163a6e7953a03eb6db084126d23ab9cec
Mark Bennett <bennettwmark@gmail.com>
2021-02-16 10:14:57 -0800

    TODO: Remove
    Adds temporary knex connection logging to test codebuild and DB connection.

--- server/db/knex.js
+++ server/db/knex.js
@@ -1,4 +1,11 @@
 var environment = process.env.NODE_ENV || 'development';
 var config = require('../knexfile.js')[environment]
+var knex = require('knex')(config);
 
-module.exports = require('knex')(config);
+try {
+  knex('eventTypes').columnInfo().then((info) => console.log("EventTypes info: ", info));
+} catch(e) {
+  console.error(e)
+}
+
+module.exports = knex;
