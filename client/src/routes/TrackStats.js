96817642cbd8cf45f2a0f90fcce835fe73e3a221
Mark Bennett <bennettwmark@gmail.com>
2021-03-14 20:09:22 -0700

    Display track feedback responses
    Slightly improved display for feedback responses

--- client/src/routes/TrackStats.js
+++ client/src/routes/TrackStats.js
@@ -1,8 +1,17 @@
-import { useState, useEffect } from 'react';
-import { useRouteMatch } from 'react-router-dom';
-import axios from 'axios';
+import { useState, useEffect } from "react";
+import { useRouteMatch } from "react-router-dom";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from "@material-ui/core";
+import axios from "axios";
 
-import PageWrapper from '../PageWrapper';
+import PageWrapper from "../PageWrapper";
 
 function StatDisplay({ stat }) {
   return (
@@ -13,49 +22,81 @@ function StatDisplay({ stat }) {
             <span>{key}: </span>
             <span>{val}</span>
           </div>
-        )
+        );
       })}
     </div>
-  )
+  );
 }
 
 function StatGroupDisplay({ title, stats }) {
   return (
     <div>
       <h2>{title}</h2>
-      {(!stats || stats.length === 0) && (
-        <div>No stats.</div>
-      )}
-      {stats?.map((stat) => <div key={stat.promptId}> <StatDisplay stat={stat} /> </div>)}
+      {(!stats || stats.length === 0) && <div>No stats.</div>}
+      {stats?.map((stat) => (
+        <div key={stat.promptId}>
+          {" "}
+          <StatDisplay stat={stat} />{" "}
+        </div>
+      ))}
     </div>
-  )
+  );
 }
 
 function TrackStats() {
-	let match = useRouteMatch();
-	const trackSlug = match.params.trackSlug;
-  const [stats, setStats] = useState([]);
+  let match = useRouteMatch();
+  const trackSlug = match.params.trackSlug;
+  const [stats, setStats] = useState({});
 
-	useEffect(() => {
-		async function fetchStats() {
-			try {
-				const payload = { trackSlug };
-        const res = await axios.post(`/api/stats/${trackSlug}`, payload)
-        setStats(res.data)
-			} catch(err) {
-				console.log("failed fetching stats: ", err);
-			}
-		}
-    fetchStats()
-	}, [trackSlug])
+  useEffect(() => {
+    async function fetchStats() {
+      try {
+        const payload = { trackSlug };
+        const res = await axios.post(`/api/stats/${trackSlug}`, payload);
+        setStats(res.data);
+      } catch (err) {
+        console.log("failed fetching stats: ", err);
+      }
+    }
+    fetchStats();
+  }, [trackSlug]);
   return (
     <PageWrapper>
+      <Typography variant="h2" component="h1">
+        {stats.track?.title}
+      </Typography>
+      {stats?.feedbackStats?.length > 0 && (
+        <>
+          <Typography variant="h4" component="h2">
+            Listener feedback
+          </Typography>
+          <TableContainer>
+            <Table size="small">
+              <TableHead>
+                <TableRow>
+                  <TableCell>Prompt</TableCell>
+                  <TableCell>Average rating</TableCell>
+                  <TableCell>Response count</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {stats?.feedbackStats.map((feedbackInfo) => (
+                  <TableRow key={feedbackInfo.promptId}>
+                    <TableCell>{feedbackInfo.prompt}</TableCell>
+                    <TableCell>
+                      {feedbackInfo.averageScore} / {feedbackInfo.scale}
+                    </TableCell>
+                    <TableCell>{feedbackInfo.numberOfResponses}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </>
+      )}
       <StatGroupDisplay stats={stats?.playbackStats} title="Playback stats" />
-      <StatGroupDisplay stats={stats?.feedbackStats} title="Feedback stats" />
     </PageWrapper>
-  )
+  );
 }
-
-
 
 export default TrackStats;
