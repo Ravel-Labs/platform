44484da594c28b1a1b0cea47932265d9296ddbca
Mark Bennett <bennettwmark@gmail.com>
2021-03-15 20:12:10 -0700

    Test DB connection: modify development DB url

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,27 +1,27 @@
 const defaultConfig = {
-  client: 'pg',
-  migrations : {
-    directory: __dirname + '/db/migrations',
-    tableName: 'migrations'
+  client: "pg",
+  migrations: {
+    directory: __dirname + "/db/migrations",
+    tableName: "migrations",
   },
   seeds: {
-    directory: __dirname + '/db/seeds',
-  }
-}
+    directory: __dirname + "/db/seeds",
+  },
+};
 
 module.exports = {
   test: {
     ...defaultConfig,
-    connection: 'postgres://localhost/ravel_test',
+    connection: "postgres://localhost/ravel_test",
     // connection: process.env.DATABASE_URL_TEST
   },
   development: {
     ...defaultConfig,
-    connection: 'postgres://localhost/ravel',
-    // connection: process.env.DATABASE_URL
+    // connection: "postgres://localhost/ravel",
+    connection: process.env.DATABASE_URL,
   },
   production: {
     ...defaultConfig,
     connection: process.env.DATABASE_URL,
-  }
-}
+  },
+};
