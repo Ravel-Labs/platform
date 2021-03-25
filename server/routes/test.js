7ae4782f4c544b072a8ec092cd87e647a7f8f0b7
Mark Bennett <bennettwmark@gmail.com>
2021-03-24 20:41:35 -0700

    Rename API test route
    Avoids naming collision with test files.

--- server/routes/test.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/', async function(req, res, next) {
-    res.send(`API is working properly 😬. Logged in: ${req.userEmail || 'none'}`);
-});
-
-module.exports = router;
