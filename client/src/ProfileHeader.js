f4098fa6ab7b6fc4295eb965c9ddfb70144763d4
Mark Bennett <bennettwmark@gmail.com>
2021-04-07 08:36:00 -0700

    format: prettier formatting changes to profile header
    TODO: we should both be using the same auto-formatting so we avoid these diffs.

--- client/src/ProfileHeader.js
+++ client/src/ProfileHeader.js
@@ -1,20 +1,20 @@
 import react from "react";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
-import Avatar from '@material-ui/core/Avatar';
-import Paper from '@material-ui/core/Paper';
+import Avatar from "@material-ui/core/Avatar";
+import Paper from "@material-ui/core/Paper";
 import { makeStyles } from "@material-ui/core/styles";
-import Button from '@material-ui/core/Button';
-import Box from '@material-ui/core/Box';
+import Button from "@material-ui/core/Button";
+import Box from "@material-ui/core/Box";
 import EditProfile from "./EditProfile";
 
 const useStyles = makeStyles((theme) => ({
   userInfoPaper: {
-    height: "250px"
+    height: "250px",
   },
   profileImage: {
     height: theme.spacing(15),
-    width: theme.spacing(15)
+    width: theme.spacing(15),
   },
   gridTextDisplay: {
     display: "flex",
@@ -28,93 +28,74 @@ const useStyles = makeStyles((theme) => ({
   Box: {
     display: "flex",
     justifyContent: "flex-start",
-    alignItems: "center"
-  }
+    alignItems: "center",
+  },
 }));
 
-function ProfileHeader({profileUser, tracks, profileStats}) {
+function ProfileHeader({ profileUser, tracks, profileStats }) {
   const classes = useStyles();
   return (
     <Box mx={30}>
-    <Grid container spacing={2} alignItems="stretch">
-      <Grid item xs={3}>
-        <Avatar 
-          alt="random" 
-          src="https://picsum.photos/250/250"
-          className={classes.profileImage}
-        >
-        </Avatar>
-      </Grid>
-      <Grid 
-        item 
-        xs={3}
-        className={classes.gridTextDisplay}
-      >
-        <Typography component="h2" variant="h5" className={classes.text}>
-          {tracks.length} tracks
-        </Typography>       
-      </Grid>
-      <Grid 
-        item 
-        xs={3}
-        className={classes.gridTextDisplay}
-      >
-        <Typography component="h2" variant="h5" className={classes.text}>
-          {profileStats.feedbackCount} ratings
-        </Typography>
-      </Grid>
-      <Grid 
-        item 
-        xs={3}
-        className={classes.gridTextDisplay}
-      >
-        {profileStats.feedbackCount > 0 ?
-          (
-            <Typography component="h2" variant="h5" className={classes.text}> 
-            {profileStats.avgRating} rating              
+      <Grid container spacing={2} alignItems="stretch">
+        <Grid item xs={3}>
+          <Avatar
+            alt="random"
+            src="https://picsum.photos/250/250"
+            className={classes.profileImage}
+          ></Avatar>
+        </Grid>
+        <Grid item xs={3} className={classes.gridTextDisplay}>
+          <Typography component="h2" variant="h5" className={classes.text}>
+            {tracks.length} tracks
+          </Typography>
+        </Grid>
+        <Grid item xs={3} className={classes.gridTextDisplay}>
+          <Typography component="h2" variant="h5" className={classes.text}>
+            {profileStats.feedbackCount} ratings
+          </Typography>
+        </Grid>
+        <Grid item xs={3} className={classes.gridTextDisplay}>
+          {profileStats.feedbackCount > 0 ? (
+            <Typography component="h2" variant="h5" className={classes.text}>
+              {profileStats.avgRating} rating
             </Typography>
-          )
-         :
-         (
-          <>
-            <Typography component="h2" variant="h5" className={classes.text}> 
-              no rating, yet             
-            </Typography>
-          </>
-        )}
-      </Grid>
-      <Grid item xs={12}>
-        <Typography component="h2" variant="h4" align="left">
-          {profileUser.displayName}
-        </Typography>
-        <Typography component="h3" variant="h5" align="left">
-          {profileUser.username}
-        </Typography>
-        <Typography component="h3" variant="h5" align="left">
-          Location
-        </Typography>
-        {profileUser.bio ? 
-          (
+          ) : (
             <>
-            {profileUser.bio}
+              <Typography component="h2" variant="h5" className={classes.text}>
+                no rating, yet
+              </Typography>
             </>
-          ) :
-          (
-          <>
-            <Typography component="h1" variant="body2" align="left">
-              I'm a real one and an early supporter of Ravel, but I don't have a bio, yet. It's on the way!
-            </Typography>
-          </>
           )}
-        <Typography component="h1" variant="body1" align="left">
-          Links
-        </Typography>
-        <Box component="span" className={classes.Box}>
-          {/*<Button variant="outlined">Edit Profile</Button>*/}
-          <EditProfile profileUser={profileUser} />
-        </Box>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography component="h2" variant="h4" align="left">
+            {profileUser.displayName}
+          </Typography>
+          <Typography component="h3" variant="h5" align="left">
+            {profileUser.username}
+          </Typography>
+          <Typography component="h3" variant="h5" align="left">
+            Location
+          </Typography>
+          {profileUser.bio ? (
+            <>{profileUser.bio}</>
+          ) : (
+            <>
+              <Typography component="h1" variant="body2" align="left">
+                I'm a real one and an early supporter of Ravel, but I don't have
+                a bio, yet. It's on the way!
+              </Typography>
+            </>
+          )}
+          <Typography component="h1" variant="body1" align="left">
+            Links
+          </Typography>
+          <Box component="span" className={classes.Box}>
+            {/*<Button variant="outlined">Edit Profile</Button>*/}
+            <EditProfile profileUser={profileUser} />
+          </Box>
+        </Grid>
       </Grid>
-    </Grid>
     </Box>
   );
 }
