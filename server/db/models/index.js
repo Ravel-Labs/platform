1ab29b59cd9e1fe3ee97e1ed43e5a70bf1aa84d8
Garrett Ransom <g2ransom@gmail.com>
2021-04-19 16:18:30 -0400

    added db models for comments api resources

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,3 +1,4 @@
+var Comments = require("./comments");
 var Events = require("./events");
 var Feedback = require("./feedback");
 var FeedbackPrompts = require("./feedbackPrompts");
@@ -13,6 +14,7 @@ var User = require("./users");
 
 
 module.exports = {
+  Comments,
   Events,
   Feedback,
   FeedbackPrompts,
